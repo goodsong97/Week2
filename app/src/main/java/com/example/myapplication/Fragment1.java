fe6a318611f3862e83b491ffb425e4a1f8898be0
goodsong97 <goodsong97@kaist.ac.kr>
2018-07-03 10:50:47 +0900

    Update Fragment2

--- app/src/main/java/com/example/myapplication/Fragment1.java
+++ app/src/main/java/com/example/myapplication/Fragment1.java
@@ -19,10 +19,6 @@ import android.support.v4.content.ContextCompat;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-
-import java.util.List;
 
 // Request code for READ_CONTACTS. It can be any number > 0.
 
@@ -106,101 +102,5 @@ public class Fragment1 extends Fragment {
             }
         }
     }
-
-
-    /*private void showContacts() {
-        // Check the SDK version and whether the permission is already granted or not.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
-            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
-        } else {
-            // Android version is lesser than 6.0 or the permission is already granted.
-            List<String> contacts = getContactNames();
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contacts);
-            lstNames.setAdapter(adapter);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                                           int[] grantResults) {
-        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission is granted
-                showContacts();
-            } else {
-                Toast.makeText(this, "Until you grant the permission, we canot display the names", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }*/
-    /*class LoadContactsAyscn extends AsyncTask<Void, Void, ArrayList<String>> {
-
-        @Override
-        protected void onPreExecute() {
-            // TODO Auto-generated method stub
-            super.onPreExecute();
-
-        }
-
-        @Override
-        protected ArrayList<String> doInBackground(Void... params) {
-            // TODO Auto-generated method stub
-            ArrayList<String> contacts = new ArrayList<String>();
-
-            Cursor c = getActivity().getContentResolver().query(
-                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
-                    null, null, null);
-            while (c.moveToNext()) {
-
-                String contactName = c
-                        .getString(c
-                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                String phNumber = c
-                        .getString(c
-                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-                contacts.add(contactName + ":" + phNumber);
-
-            }
-            c.close();
-
-            return contacts;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<String> contacts) {
-            // TODO Auto-generated method stub
-            super.onPostExecute(contacts);
-
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                    getActivity().getApplicationContext(), R.layout.text, contacts);
-
-            list.setAdapter(adapter);
-
-        }
-
-    }*/
-
-    /*@Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_fragment1,
-
-                container,false);
-        ListView listView = (ListView) view.findViewById(R.id.list_view);
-        String[] menuItems ={"1", "2", "3"};
-        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                menuItems
-        );
-
-        listView.setAdapter(listViewAdapter);
-
-        return view;
-    }*/
 
 }
--- app/src/main/java/com/example/myapplication/Fragment2.java
+++ app/src/main/java/com/example/myapplication/Fragment2.java
@@ -1,31 +1,177 @@
 package com.example.myapplication;
+import java.util.ArrayList;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.provider.MediaStore;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.widget.Toast;
+
 
 public class Fragment2 extends Fragment {
     public Fragment2()
     {
         // required
     }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-
                              @Nullable Bundle savedInstanceState) {
-        RelativeLayout layout = (RelativeLayout)inflater.inflate(R.layout.fragment_fragment2,
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_fragment2, container,false);
+        GridView gv = (GridView) view.findViewById(R.id.ImgGridView);
+        final ImageAdapter ia = new ImageAdapter(getActivity());
+        gv.setAdapter(ia);
+        gv.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                Bundle bundle = new Bundle();
+                //Intent i = new Intent(getActivity(),ImagePopup.class);
+                String imgPath = ia.getImageInfo(ia.imgData, ia.geoData, ia.thumbsIDList.get(position));
+                bundle.putString("filename", imgPath);
+                //i.putExtra("filename", imgPath);
+                ImagePopup f = new ImagePopup();
+                f.setArguments(bundle);
+                FragmentManager fmanager = getFragmentManager();
+                FragmentTransaction ftrans = fmanager.beginTransaction();
+                ftrans.replace(R.id.pager,f).addToBackStack(f.getClass().getName());
+                ftrans.commit();
+            }
+        });
+        return view;
+    }
 
-                container,false);
-        return layout;
+    public class ImageAdapter extends BaseAdapter {
+        private String imgData;
+        private String geoData;
+        private ArrayList<String> thumbsDataList;
+        private ArrayList<String> thumbsIDList;
+
+        ImageAdapter(Context c){
+            thumbsDataList = new ArrayList<String>();
+            thumbsIDList = new ArrayList<String>();
+            getThumbInfo(thumbsIDList, thumbsDataList);
+        }
+
+        public final Fragment callImageViewer(int selectedIndex){
+            Bundle bundle = new Bundle();
+            //Intent i = new Intent(getActivity(),ImagePopup.class);
+            String imgPath = getImageInfo(imgData, geoData, thumbsIDList.get(selectedIndex));
+            bundle.putString("filename", imgPath);
+            //i.putExtra("filename", imgPath);
+            ImagePopup f = new ImagePopup();
+            f.setArguments(bundle);
+            return f;
+        }
+
+        public boolean deleteSelected(int sIndex){
+            return true;
+        }
+
+        public int getCount() {
+            return thumbsIDList.size();
+        }
+
+        public Object getItem(int position) {
+            return position;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null){
+                imageView = new ImageView(getActivity());
+                imageView.setLayoutParams(new GridView.LayoutParams(250, 250));
+                imageView.setAdjustViewBounds(false);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(2, 2, 2, 2);
+            }else{
+                imageView = (ImageView) convertView;
+            }
+            BitmapFactory.Options bo = new BitmapFactory.Options();
+            bo.inSampleSize = 8;
+            Bitmap bmp = BitmapFactory.decodeFile(thumbsDataList.get(position), bo);
+            Bitmap resized = Bitmap.createScaledBitmap(bmp, 250, 250, true);
+            imageView.setImageBitmap(resized);
+
+            return imageView;
+        }
+
+        private void getThumbInfo(ArrayList<String> thumbsIDs, ArrayList<String> thumbsDatas){
+            String[] proj = {MediaStore.Images.Media._ID,
+                    MediaStore.Images.Media.DATA,
+                    MediaStore.Images.Media.DISPLAY_NAME,
+                    MediaStore.Images.Media.SIZE};
+
+            Cursor imageCursor = getActivity().managedQuery(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    proj, null, null, null);
+
+            if (imageCursor != null && imageCursor.moveToFirst()){
+                String title;
+                String thumbsID;
+                String thumbsImageID;
+                String thumbsData;
+                String data;
+                String imgSize;
+
+                int thumbsIDCol = imageCursor.getColumnIndex(MediaStore.Images.Media._ID);
+                int thumbsDataCol = imageCursor.getColumnIndex(MediaStore.Images.Media.DATA);
+                int thumbsImageIDCol = imageCursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME);
+                int thumbsSizeCol = imageCursor.getColumnIndex(MediaStore.Images.Media.SIZE);
+                int num = 0;
+                do {
+                    thumbsID = imageCursor.getString(thumbsIDCol);
+                    thumbsData = imageCursor.getString(thumbsDataCol);
+                    thumbsImageID = imageCursor.getString(thumbsImageIDCol);
+                    imgSize = imageCursor.getString(thumbsSizeCol);
+                    num++;
+                    if (thumbsImageID != null){
+                        thumbsIDs.add(thumbsID);
+                        thumbsDatas.add(thumbsData);
+                    }
+                }while (imageCursor.moveToNext());
+            }
+            imageCursor.close();
+            return;
+        }
+
+        private String getImageInfo(String ImageData, String Location, String thumbID){
+            String imageDataPath = null;
+            String[] proj = {MediaStore.Images.Media._ID,
+                    MediaStore.Images.Media.DATA,
+                    MediaStore.Images.Media.DISPLAY_NAME,
+                    MediaStore.Images.Media.SIZE};
+            Cursor imageCursor = getActivity().managedQuery(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    proj, "_ID='"+ thumbID +"'", null, null);
+
+            if (imageCursor != null && imageCursor.moveToFirst()){
+                if (imageCursor.getCount() > 0){
+                    int imgData = imageCursor.getColumnIndex(MediaStore.Images.Media.DATA);
+                    imageDataPath = imageCursor.getString(imgData);
+                }
+            }
+            imageCursor.close();
+            return imageDataPath;
+        }
     }
 }
