6c546526751093008067b25fd5d490cead597714
goodsong97 <goodsong97@kaist.ac.kr>
2018-07-05 02:54:07 +0900

    진짜 합친거

--- app/src/main/java/com/example/myapplication/Fragment3.java
+++ app/src/main/java/com/example/myapplication/Fragment3.java
@@ -1,11 +1,18 @@
 package com.example.myapplication;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+
 
 public class Fragment3 extends Fragment {
     public Fragment3()
@@ -25,7 +32,17 @@ public class Fragment3 extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         RelativeLayout layout = (RelativeLayout)inflater.inflate(R.layout.fragment_fragment3,
 
-                container,false);
+                container, false);
+
+        Button start_but = (Button) layout.findViewById(R.id.start);
+        start_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Game.class);
+                startActivity(intent);
+            }
+        });
+
         return layout;
     }
 }
